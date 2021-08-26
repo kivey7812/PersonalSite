dd6c45afa596deb4b1952d195b2ac68411c352b0
kivey7812 <kirsten.ivey@gmail.com>
2021-08-26 09:42:12 -0700

    update link to style sheet

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("Public"));
 
 app.get("/", function(req, res){
   res.render("index");
